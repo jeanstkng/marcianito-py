6033ef3952436f31be548c74878baf687594db93
JeanStkng <jeancast.jg@gmail.com>
2024-04-14 22:38:39 -0500

    Laser increase and moves to mouse

--- laser_beam.py
+++ laser_beam.py
@@ -11,29 +11,26 @@ class LaserBeam():
         self.x_scale = 10
         self.x_pos_increment = 5
         self.laser = pygame.image.load(os.path.join('images','laser.png')).convert_alpha()
-        self.laser = pygame.transform.scale(self.laser, (self.x_scale, 20))
         self.pivot = pivot
-        self.pos = Vector2(pivot) + (self.x_pos_increment, 0) # 5 por cada 10 en escala
+        self.pos = Vector2(pivot) + (self.x_pos_increment, 0)    
+        self.mouse_pos = Vector2(pygame.mouse.get_pos())
         
-        self.image_orig = self.laser
-        self.image_unflipped = self.image_orig
-        self.image_flipped = pygame.transform.flip(self.image_orig, False, True)
+        self.handle_image_change_by_mouse_pos(self.pivot, self.mouse_pos)
         
-        self.image = self.image_orig
         self.rect = self.image.get_rect(center = self.pos)
+        
 
     def update(self, pivot):
-        mouse_pos = Vector2(pygame.mouse.get_pos())
-        self.pivot = pivot
-        self.pos = Vector2(pivot) + (self.x_pos_increment, 0) # 5 por cada 10 en escala
-        self.rect = self.image.get_rect(center = self.pos)
+        self.mouse_pos = Vector2(pygame.mouse.get_pos())
         
-        if mouse_pos.x < screen_center_x:
+        self.handle_image_change_by_mouse_pos(pivot, self.mouse_pos)
+        
+        if self.mouse_pos.x < screen_center_x:
             self.image_orig = self.image_flipped
         else:
             self.image_orig = self.image_unflipped
             
-        mouse_offset = mouse_pos - self.pivot
+        mouse_offset = self.mouse_pos - self.pivot
         mouse_angle = -math.degrees(math.atan2(mouse_offset.y, mouse_offset.x))
         
         self.image, self.rect = self.rotate_on_pivot(self.image_orig, mouse_angle, self.pivot, self.pos)
@@ -48,3 +45,19 @@ class LaserBeam():
         rect = surf.get_rect(center = offset)
         
         return surf, rect
+    
+    def handle_image_change_by_mouse_pos(self, pivot, mouse_pos):
+        self.pivot = pivot
+        
+        distance = mouse_pos.distance_to(Vector2(self.pivot))
+
+        self.x_scale = int(distance)
+        self.x_pos_increment = int(distance / 2)
+
+        self.image_orig = pygame.transform.scale(self.laser, (self.x_scale, 20))
+        self.image = self.image_orig
+        self.image_unflipped = pygame.transform.scale(self.laser, (self.x_scale, 20))
+        self.image_flipped = pygame.transform.flip(self.image_orig, False, True)
+
+        self.pos = Vector2(self.pivot) + (self.x_pos_increment, 0)
+        self.rect = self.image.get_rect(center = self.pos)
